8e3348641f6311e99b03e79c8fb9cfd30b6d3d2d
Joe McCleary <30754844+joemccleary-frontend@users.noreply.github.com>
2021-04-29 15:53:22 +0100

    added server files

---
+++ index.js
@@ -0,0 +1,61 @@
+const dotenv = require('dotenv');
+dotenv.config();
+var path = require('path')
+const express = require('express')
+const fetch = require('node-fetch');
+
+const app = express()
+app.use(express.static('dist'))
+require("babel-polyfill");
+
+// Install cors
+const cors = require('cors');
+app.use(cors());
+
+//middleware
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/', function (res) {
+    res.sendFile('dist/index.html')
+})
+
+app.get("/test", function(req, res) {
+    res.json({
+        status: 200,
+    });
+});
+//get keys
+const geonameKey = process.env.geonames;
+const weatherbitKey = process.env.weatherbit;
+const pixabayKey = process.env.pixabay;
+//Post data
+app.post('https://postcardyourfuture.netlify.app/post', async(req, res) => {
+    const coords = await getCoords(req.body.city);
+    async function getCoords(location) {
+        const geo = await fetch(`http://api.geonames.org/searchJSON?formatted=true&q=${location}&maxRows=5&lang=en&username=${geonameKey}`);
+        return geo.json();
+    }
+    const weather = await getWeather(coords.geonames[0].lng, coords.geonames[0].lat);
+    async function getWeather(lon, lat) {
+        const weather = await fetch(`https://api.weatherbit.io/v2.0/current?lon=${lon}&lat=${lat}&key=${weatherbitKey}`);
+        return weather.json();
+    }
+    const country = await getCountry(coords.geonames[0].countryCode);
+    async function getCountry(countryCode) {
+        const country = await fetch(`https://restcountries.eu/rest/v2/alpha/${countryCode}`);
+        return country.json();
+    }
+    const imageData = await getImage(req.body.city);
+    async function getImage(location) {
+        const image = await fetch(`https://pixabay.com/api/?key=${pixabayKey}&q=${location}&image_type=photo`);
+        return image.json();
+    }
+    res.send(JSON.stringify({ 
+        weatherInfo: weather,  
+        country: country,
+        image: imageData,
+    }))
+});
+module.exports = app;
